14a29eacbf460585af9089e85395f42924ebcc76
crisstanza <crisstanza@home>
2020-02-02 22:52:57 -0300

    C major and D major.

---
+++ chords-piano.js
@@ -0,0 +1,14 @@
+var CHORDS = [
+	{
+		"chord": "C", "triad": [ 0, 4, 7 ],
+		"inversions": [
+			[1, 4, 9], [1, 6, 10]
+		]
+	},
+	{
+		"chord": "D", "triad": [ 2, 6, 9 ],
+		"inversions": [
+			[3, 6, 11], [3, 8, 12]
+		]
+	},
+];
