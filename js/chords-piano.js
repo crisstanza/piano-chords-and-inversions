e768436149d51442561afe38f65b5439494e6b09
crisstanza <crisstanza@home>
2020-02-06 22:34:21 -0300

    Não Aprendi Dizer Adeus - Leandro & Leonardo
    Acordes da música Não Aprendi Dizer Adeus - Leandro & Leonardo: G, C, D, Bm e Em.

--- js/chords-piano.js
+++ js/chords-piano.js
@@ -7,17 +7,50 @@ var KEYBOARDS = {
 
 var CHORDS = [
 	{
+		"chord": "G", "triad": [ 2, 7, 11 ],
+		"inversions": [
+			[ 4, 8, 11 ], [ 5, 8, 13 ]
+		],
+		"triadInversions": [
+			[ 7, 11, 2 ], [ 11, 2, 7 ]
+		]
+	},
+	{
 		"chord": "C", "triad": [ 0, 4, 7 ],
 		"inversions": [
 			[ 1, 4, 9 ], [ 1, 6, 10 ]
+		],
+		"triadInversions": [
+			[ 4, 7, 0 ], [ 7, 0, 4 ]
 		]
 	},
 	{
 		"chord": "D", "triad": [ 2, 6, 9 ],
 		"inversions": [
 			[ 3, 6, 11 ], [ 3, 8, 12 ]
+		],
+		"triadInversions": [
+			[ 6, 9, 2 ], [ 9, 2, 6 ]
 		]
 	},
+	{
+		"chord": "Bm", "triad": [ 2, 6, 11 ],
+		"inversions": [
+			[ 8, 3, 11 ], [ 5, 8, 12 ]
+		],
+		"triadInversions": [
+			[ 6, 11, 2 ], [ 11, 2, 6 ]
+		]
+	},
+	{
+		"chord": "Em", "triad": [ 4, 7, 11 ],
+		"inversions": [
+			[ 4, 8, 13 ], [ 5, 10, 13 ]
+		],
+		"triadInversions": [
+			[ 7, 11, 4 ], [ 11, 4, 7 ]
+		]
+	}
 ];
 
 var NOTES = [ 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B' ];
--- js/index.js
+++ js/index.js
@@ -1,9 +1,17 @@
 (function() {
 
-	var CREATOR;
+	let CREATOR;
+
+	let KEY_WHITE_WIDTH = 30;
+	let KEY_BLACK_WIDTH = 20;
+
+	let KEY_WHITE_HEIGHT = 80;
+	let KEY_BLACK_HEIGHT = 50;
+
+	let KEY_CURRENT_RADIUS = 6;
 
 	function drawChord(chord) {
-		let table = CREATOR.create('table', { border: 1 });
+		let table = CREATOR.create('table', { border: 0 });
 
 		let tHead = CREATOR.create('thead', {}, table);
 		let headRow = tHead.insertRow(0);
@@ -20,10 +28,10 @@
 
 		let tFoot = CREATOR.create('tfoot', {}, table);
 		let footRow = tFoot.insertRow(0);
-		drawTriad(footRow, chord.triad);
-		for (let i = 0 ; i < chord.inversions.length ; i++) {
-			let triad = chord.inversions[i];
-			drawTriad(footRow, triad);
+		drawTriad(footRow, chord.triad, chord.chord[0]);
+		for (let i = 0 ; i < chord.triadInversions.length ; i++) {
+			let triad = chord.triadInversions[i];
+			drawTriad(footRow, triad, chord.chord[0]);
 		}
 
 		main.appendChild(table);
@@ -31,11 +39,51 @@
 
 	function drawKeyboard(parentRow, keyboard, triad) {
 		let col = CREATOR.create('td', {}, parentRow);
-		let svg = CREATOR.create.svg('svg', {}, col);
+		let svg = CREATOR.create.svg('svg', {height: KEY_WHITE_HEIGHT + 2}, col);
+		let delta = keyboard[0] == 'w' ? 0 : KEY_BLACK_WIDTH / 2;
+		let x;
+		for (let i = 0, j = 0 ; i < keyboard.length ; i++) {
+			let key = keyboard[i];
+			if (key == 'w') {
+				x = 1 + delta + KEY_WHITE_WIDTH * j++;
+				CREATOR.create.svg('rect', {x: x, y: 1, width: KEY_WHITE_WIDTH, height: KEY_WHITE_HEIGHT, stroke: '#000', fill: '#F7F4F1', 'stroke-width': 1}, svg);
+				if (i == keyboard.length - 1) {
+					let svgWidth = x + KEY_WHITE_WIDTH + 1;
+					svg.setAttribute('width', svgWidth);
+				}
+				if (triad.includes(i)) {
+					CREATOR.create.svg('circle', {cx: x + KEY_WHITE_WIDTH / 2, cy: KEY_WHITE_HEIGHT - 12, r: KEY_CURRENT_RADIUS, height: 90, stroke: '#222', fill: '#666', 'stroke-width': 1}, svg);
+				}
+			}
+		}
+		for (let i = 0, j = 0 ; i < keyboard.length ; i++) {
+			let key = keyboard[i];
+			if (key == 'w') {
+				j++;
+			} else if (key == 'b') {
+				x = 1 + delta + KEY_WHITE_WIDTH * j - KEY_BLACK_WIDTH / 2;
+				CREATOR.create.svg('rect', {x: x, y: 1, width: KEY_BLACK_WIDTH, height: KEY_BLACK_HEIGHT, stroke: '#000', fill: '#AAA', 'stroke-width': 1}, svg);
+				if (i == keyboard.length - 1) {
+					let svgWidth = x + KEY_BLACK_WIDTH + 1;
+					svg.setAttribute('width', svgWidth);
+				}
+				if (triad.includes(i)) {
+					CREATOR.create.svg('circle', {cx: x + KEY_BLACK_WIDTH / 2, cy: KEY_BLACK_HEIGHT - 12, r: KEY_CURRENT_RADIUS, height: 90, stroke: '#222', fill: '#666', 'stroke-width': 1}, svg);
+				}
+			}
+		}
 	}
 
-	function drawTriad(parentRow, triad) {
-		let col = CREATOR.create('td', {}, parentRow, triad);
+	function drawTriad(parentRow, triad, root) {
+		let className = NOTES[triad[0] % 12] == root ? 'bold' : '';
+		let col = CREATOR.create('td', {class: className}, parentRow);
+		for (let i = 0 ; i < triad.length ; i++) {
+			let note = triad[i];
+			let noteName = NOTES[note % 12];
+			className = noteName == root ? 'italic' : '';
+			col.innerHTML += '<span class="' + className + '">' + noteName + '</span> ';
+		}
+		col.innerHTML = col.innerHTML.trim();
 	}
 
 	function drawChords(chords) {
