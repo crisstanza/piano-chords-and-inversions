ed30dde2db5706cc6cc0606ad4a1f4f2393b08aa
Cris Stanza <crisstanza@users.noreply.github.com>
2021-02-26 17:05:06 -0300

    Chico Buarque - Desalento (piano).

--- js/Chico-Buarque_Desalento.js
+++ js/Chico-Buarque_Desalento.js
@@ -73,7 +73,7 @@ var CHORDS_TMP = [
 				"chord": "F(7m)/A",
 				"notes": [ [ [4, 11, 16, 19] ] ],
 				"fingers": [ [3, 1, 2, 3] ],
-				"names": [ [-10, '', 12, 4, 7] ],
+				"names": [ [9, '', 4, 9, 12] ],
 				"keyboards": [7],
 			},
 			{
@@ -120,9 +120,82 @@ var CHORDS_TMP = [
 			{
 				"chord": "F(7M)",
 				"notes": [ [ [0, 11, 16, 19] ] ],
-				"fingers": [ [5, 1, 2, 3] ],
+				"fingers": [ [4, 1, 2, 3] ],
 				"names": [ [5, '', 4, 9, 12] ],
 				"keyboards": [7]
+			},
+			{
+				"chord": "Dm(7m)",
+				"notes": [ [ [2, 17, 21, 24] ] ],
+				"fingers": [ [5, 1, 2, 3] ],
+				"names": [ [2, '', 5, 9, 12] ],
+				"keyboards": [4]
+			},
+			{
+				"chord": "D#º",
+				"notes": [ [ [3, 18, 21, 24] ] ],
+				"fingers": [ [4, 1, 2, 3] ],
+				"names": [ [5, '', 4, 9, 12] ],
+				"keyboards": [4]
+			},
+			{
+				"chord": "Em(7m)(5-)",
+				"notes": [ [ [4, 14, 19, 22] ] ],
+				"fingers": [ [5, 1, 2, 3] ],
+				"names": [ [4, '', 2, 7, -10] ],
+				"keyboards": [4]
+			},
+			{
+				"chord": "Gm(6)/Bb",
+				"notes": [ [ [5, 11, 14, 21] ] ],
+				"fingers": [ [3, 1, 2, 4] ],
+				"names": [ [-10, '', 4, 7, 2] ],
+				"keyboards": [7]
+			},
+			{
+				"chord": "A(7m)(9-)",
+				"notes": [ [ [4, 14, 17, 20] ] ],
+				"fingers": [ [3, 2, 3, 4] ],
+				"names": [ [9, '', 7, -10, 1] ],
+				"keyboards": [7]
+			},
+			{
+				"chord": "A/G",
+				"notes": [ [ [2, 11, 16, 20] ] ],
+				"fingers": [ [3, 1, 2, 4] ],
+				"names": [ [7, '', 4, 9, 1] ],
+				"keyboards": [7]
+			},
+			{
+				"spacer": { "type": "break-line" }
+			},
+			{
+				"chord": "Dm/A",
+				"notes": [ [ [4, 9, 12, 16] ] ],
+				"fingers": [ [3, 1, 2, 3] ],
+				"names": [ [9, '', 2, 5, 9] ],
+				"keyboards": [7]
+			},
+			{
+				"chord": "B/A",
+				"notes": [ [ [4, 18, 22, 25] ] ],
+				"fingers": [ [3, 1, 2, 4] ],
+				"names": [ [9, '', 11, 3, 6] ],
+				"keyboards": [5]
+			},
+			{
+				"chord": "Am(7m)(5-)",
+				"notes": [ [ [4, 19, 22, 26] ] ],
+				"fingers": [ [3, 1, 2, 4] ],
+				"names": [ [9, '', 12, -3, 7] ],
+				"keyboards": [5]
+			},
+			{
+				"chord": "Dm(7m)",
+				"notes": [ [ [2, 12, 17, 21] ] ],
+				"fingers": [ [3, 1, 2, 4] ],
+				"names": [ [2, '', 12, 5, 9] ],
+				"keyboards": [6]
 			}
 		]
 	}
